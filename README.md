5706a62c982c4d34350102882ac1847925ceab5f
gaohaixia1011 <3468685370@qq.com>
2026-03-22 22:48:54 +0800

    重命名 lab02 和 lab03 文件夹为 ex02 和 ex03

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ programming homework
 
 ”高海霞“
 
+\## 版本更新记录
+
+\- v1.0：初始版本，输出 Hello World
+
+\- v2.0：增加 input 输入功能，可以显示用户输入的名字
+
+
+
--- lab01/sketch_mar13b/sketch_mar13b.ino
+++ lab01/sketch_mar13b/sketch_mar13b.ino
@@ -1,15 +1,17 @@
+// 定义板载LED引脚，避免使用"魔法数字"
 #define LED_PIN 2
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(LED_PIN, OUTPUT);
-    Serial.println("ESP32 启动成功！");
+  // 初始化串口通信
+  Serial.begin(115200);
+  // 初始化板载LED引脚为输出模式
+  pinMode(LED_PIN, OUTPUT); 
 }
 
 void loop() {
-    Serial.println("Hello ESP32!");
-    digitalWrite(LED_PIN, HIGH); // 点亮LED
-    delay(1000);                 // 亮1秒
-    digitalWrite(LED_PIN, LOW);  // 熄灭LED  ← 这一行必须加上
-    delay(1000);                 // 灭1秒
+  Serial.println("Hello ESP32!");
+  digitalWrite(LED_PIN, HIGH);   // 点亮LED
+  delay(1000);                   // 等待1秒
+  digitalWrite(LED_PIN, LOW);    // 熄灭LED
+  delay(1000);              // 等待1秒
 }
